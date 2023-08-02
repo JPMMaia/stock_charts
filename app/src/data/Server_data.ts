cbb29295a31fd241d78e23c930c769af1570fb91
João Maia <jpmmaia@gmail.com>
2023-08-02 20:29:42 +0300

    Retrieve data and parse

--- app/src/data/Server_data.ts
+++ app/src/data/Server_data.ts
@@ -19,3 +19,179 @@ export interface Time_series_data {
     meta_data: Meta_data;
     entries: Time_series_data_entry[];
 }
+
+export interface Overview_data {
+    symbol: string;
+    asset_type: string;
+    name: string;
+    description: string;
+    cik: number;
+    exchange: string;
+    currency: string;
+    country: string;
+    sector: string;
+    industry: string;
+    address: string;
+    fiscal_year_end: string;
+    latest_quarter: Date;
+    market_capitalization: number;
+    ebitda: number;
+    pe_ratio: number;
+    peg_ratio: number;
+    book_value: number;
+    dividend_per_share: number;
+    dividend_yield: number;
+    eps: number;
+    revenue_per_share_ttm: number;
+    profit_margin: number;
+    operating_margin_ttm: number;
+    return_on_assets_ttm: number;
+    return_on_equity_ttm: number;
+    revenue_ttm: number;
+    gross_profit_ttm: number;
+    diluted_epsttm: number;
+    quarterly_earnings_growth_yoy: number;
+    quarterly_revenue_growth_yoy: number;
+    analyst_target_price: number;
+    trailing_pe: number;
+    forward_pe: number;
+    price_to_sales_ratio_ttm: number;
+    price_to_book_ratio: number;
+    ev_to_revenue: number;
+    ev_to_ebitda: number;
+    beta: number;
+    price_52_week_high: number;
+    price_52_week_low: number;
+    price_50_day_moving_average: number;
+    price_200_day_moving_average: number;
+    shares_outstanding: number;
+    dividend_date: Date;
+    ex_dividend_date: Date;
+}
+
+export interface Income_statement_data_entry {
+    fiscal_date_ending: Date;
+    reported_currency: string;
+    gross_profit: number;
+    total_revenue: number;
+    cost_of_revenue: number;
+    costof_goods_and_services_sold: number;
+    operating_income: number;
+    selling_general_and_administrative: number;
+    research_and_development: number;
+    operating_expenses: number;
+    investment_income_net: number;
+    net_interest_income: number;
+    interest_income: number;
+    interest_expense: number;
+    non_interest_income: number;
+    other_non_operating_income: number;
+    depreciation: number;
+    depreciation_and_amortization: number;
+    income_before_tax: number;
+    income_tax_expense: number;
+    interest_and_debt_expense: number;
+    net_income_from_continuing_operations: number;
+    comprehensive_income_net_of_tax: number;
+    ebit: number;
+    ebitda: number;
+    net_income: number;
+}
+
+export interface Income_statement_data {
+    symbol: string;
+    annual_reports: Income_statement_data_entry[];
+}
+
+export interface Balance_sheet_data_entry {
+    fiscal_date_ending: Date;
+    reported_currency: string;
+    total_assets: number;
+    total_current_assets: number;
+    cash_and_cash_equivalents_at_carrying_value: number;
+    cash_and_short_term_investments: number;
+    inventory: number;
+    current_net_receivables: number;
+    total_non_current_assets: number;
+    property_plant_equipment: number;
+    accumulated_depreciation_amortization_ppe: number;
+    intangible_assets: number;
+    intangible_assets_excluding_goodwill: number;
+    goodwill: number;
+    investments: number;
+    long_term_investments: number;
+    short_term_investments: number;
+    other_current_assets: number;
+    other_non_current_assets: number;
+    total_liabilities: number;
+    total_current_liabilities: number;
+    current_accounts_payable: number;
+    deferred_revenue: number;
+    current_debt: number;
+    short_term_debt: number;
+    total_non_current_liabilities: number;
+    capital_lease_obligations: number;
+    long_term_debt: number;
+    current_long_term_debt: number;
+    long_term_debt_noncurrent: number;
+    short_long_term_debt_total: number;
+    other_current_liabilities: number;
+    other_non_current_liabilities: number;
+    total_shareholder_equity: number;
+    treasury_stock: number;
+    retained_earnings: number;
+    common_stock: number;
+    common_stock_shares_outstanding: number;
+}
+
+export interface Balance_sheet_data {
+    symbol: string;
+    annual_reports: Balance_sheet_data_entry[];
+}
+
+export interface Cash_flow_data_entry {
+    fiscal_date_ending: Date;
+    reported_currency: string;
+    operating_cashflow: number,
+    payments_for_operating_activities: number,
+    proceeds_from_operating_activities: number,
+    change_in_operating_liabilities: number,
+    change_in_operating_assets: number,
+    depreciation_depletion_and_amortization: number,
+    capital_expenditures: number,
+    change_in_receivables: number,
+    change_in_inventory: number,
+    profit_loss: number,
+    cashflow_from_investment: number,
+    cashflow_from_financing: number,
+    proceeds_from_repayments_of_short_term_debt: number,
+    payments_for_repurchase_of_common_stock: number,
+    payments_for_repurchase_of_equity: number,
+    payments_for_repurchase_of_preferred_stock: number,
+    dividend_payout: number,
+    dividend_payout_common_stock: number,
+    dividend_payout_preferred_stock: number,
+    proceeds_from_issuance_of_common_stock: number,
+    proceeds_from_issuance_of_long_term_debt_and_capital_securities_net: number,
+    proceeds_from_issuance_of_preferred_stock: number,
+    proceeds_from_repurchase_of_equity: number,
+    proceeds_from_sale_of_treasury_stock: number,
+    change_in_cash_and_cash_equivalents: number,
+    change_in_exchange_rate: number,
+    net_income: number,
+}
+
+export interface Cash_flow_data {
+    symbol: string;
+    annual_reports: Cash_flow_data_entry[];
+}
+
+export interface Listing_status_entry {
+    symbol: string;
+    name: string;
+    exchange: string;
+    asset_type: string;
+    ipo_date: Date;
+    delisting_date: Date | null;
+    status: string;
+}
--- app/src/data/data_service.ts
+++ app/src/data/data_service.ts
@@ -3,30 +3,33 @@ import axios from 'axios';
 import * as Server_data from "./Server_data";
 
 enum Metric {
-    Time_series_montly_adjusted
+    Balance_sheet = "BALANCE_SHEET",
+    Cash_flow = "CASH_FLOW",
+    Income_statement = "INCOME_STATEMENT",
+    Listing_status = "LISTING_STATUS",
+    Overview = "OVERVIEW",
+    Time_series_montly_adjusted = "TIME_SERIES_MONTHLY_ADJUSTED",
 }
 
 function metric_to_function_name(metric: Metric): string {
-    switch (metric) {
-        case Metric.Time_series_montly_adjusted:
-            return "TIME_SERIES_MONTHLY_ADJUSTED";
-        default:
-            throw Error("Invalid metric!");
-    }
+    return metric.toString();
 }
 
 function get_base_url(): string {
     return "https://www.alphavantage.co/query";
 }
 
-function create_url(function_name: string, symbol: string, api_key: string): string {
+function create_url(function_name: string, symbol: string | undefined, api_key: string): string {
     const base_url = get_base_url();
-    return `${base_url}?function=${function_name}&symbol=${symbol}&apikey=${api_key}`;
+    if (symbol !== undefined) {
+        return `${base_url}?function=${function_name}&symbol=${symbol}&apikey=${api_key}`;
+    }
+    else {
+        return `${base_url}?function=${function_name}&apikey=${api_key}`;
+    }
 }
 
-
-
-async function get_data(symbol: string, metric: Metric, api_key: string, cache: Storage, force_fetch: boolean): Promise<unknown> {
+async function get_data(symbol: string | undefined, metric: Metric, api_key: string, cache: Storage, force_fetch: boolean): Promise<unknown> {
 
     const function_name = metric_to_function_name(metric);
     const url = create_url(function_name, symbol, api_key);
@@ -77,8 +80,80 @@ export async function get_time_series_data(symbol: string, api_key: string, cach
     return data;
 }
 
+export async function get_overview_data(symbol: string, api_key: string, cache: Storage, force_fetch: boolean): Promise<Server_data.Overview_data | undefined> {
+
+    const raw_json = await get_data(symbol, Metric.Overview, api_key, cache, force_fetch);
+
+    if (raw_json === undefined) {
+        return Promise.resolve(undefined);
+    }
+
+    const data = transform_raw_json_to_overview_data(raw_json);
+    return data;
+}
+
+export async function get_income_statement_data(symbol: string, api_key: string, cache: Storage, force_fetch: boolean): Promise<Server_data.Income_statement_data | undefined> {
+
+    const raw_json = await get_data(symbol, Metric.Income_statement, api_key, cache, force_fetch);
+
+    if (raw_json === undefined) {
+        return Promise.resolve(undefined);
+    }
+
+    const data = transform_raw_json_to_income_statement_data(raw_json);
+    return data;
+}
+
+export async function get_balance_sheet_data(symbol: string, api_key: string, cache: Storage, force_fetch: boolean): Promise<Server_data.Balance_sheet_data | undefined> {
+
+    const raw_json = await get_data(symbol, Metric.Balance_sheet, api_key, cache, force_fetch);
+
+    if (raw_json === undefined) {
+        return Promise.resolve(undefined);
+    }
+
+    const data = transform_raw_json_to_balance_sheet_data(raw_json);
+    return data;
+}
+
+export async function get_cash_flow_data(symbol: string, api_key: string, cache: Storage, force_fetch: boolean): Promise<Server_data.Cash_flow_data | undefined> {
+
+    const raw_json = await get_data(symbol, Metric.Cash_flow, api_key, cache, force_fetch);
+
+    if (raw_json === undefined) {
+        return Promise.resolve(undefined);
+    }
+
+    const data = transform_raw_json_to_cash_flow_data(raw_json);
+    return data;
+}
+
+export async function get_listing_status_data(api_key: string, cache: Storage, force_fetch: boolean): Promise<Server_data.Listing_status_entry[] | undefined> {
+
+    const csv = await get_data(undefined, Metric.Listing_status, api_key, cache, force_fetch);
+
+    if (csv === undefined) {
+        return Promise.resolve(undefined);
+    }
+
+    const data = transform_csv_to_listing_status_data(csv as string);
+    return data;
+}
+
 function parse_date(value: string, timezone: string): Date {
     return new Date(value);
+}
+
+function parse_number(value: string): number {
+    if (value === "None") {
+        return 0;
+    }
+
+    return Number(value);
+}
+
+function parse_string(value: any): string {
+    return value;
 }
 
 function transform_raw_json_to_time_series_data(json: any): Server_data.Time_series_data {
@@ -116,4 +191,245 @@ function transform_raw_json_to_time_series_data(json: any): Server_data.Time_ser
         meta_data: meta_data,
         entries: entries
     };
+}
+
+function transform_raw_json_to_overview_data(json: any): Server_data.Overview_data {
+
+    const overview_data: Server_data.Overview_data = {
+        symbol: parse_string(json["Symbol"]),
+        asset_type: parse_string(json["AssetType"]),
+        name: parse_string(json["Name"]),
+        description: parse_string(json["Description"]),
+        cik: parse_number(json["CIK"]),
+        exchange: parse_string(json["Exchange"]),
+        currency: parse_string(json["Currency"]),
+        country: parse_string(json["Country"]),
+        sector: parse_string(json["Sector"]),
+        industry: parse_string(json["Industry"]),
+        address: parse_string(json["Address"]),
+        fiscal_year_end: parse_string(json["FiscalYearEnd"]),
+        latest_quarter: parse_date(json["LatestQuarter"], ""),
+        market_capitalization: parse_number(json["MarketCapitalization"]),
+        ebitda: parse_number(json["EBITDA"]),
+        pe_ratio: parse_number(json["PERatio"]),
+        peg_ratio: parse_number(json["PEGRatio"]),
+        book_value: parse_number(json["BookValue"]),
+        dividend_per_share: parse_number(json["DividendPerShare"]),
+        dividend_yield: parse_number(json["DividendYield"]),
+        eps: parse_number(json["EPS"]),
+        revenue_per_share_ttm: parse_number(json["RevenuePerShareTTM"]),
+        profit_margin: parse_number(json["ProfitMargin"]),
+        operating_margin_ttm: parse_number(json["OperatingMarginTTM"]),
+        return_on_assets_ttm: parse_number(json["ReturnOnAssetsTTM"]),
+        return_on_equity_ttm: parse_number(json["ReturnOnEquityTTM"]),
+        revenue_ttm: parse_number(json["RevenueTTM"]),
+        gross_profit_ttm: parse_number(json["GrossProfitTTM"]),
+        diluted_epsttm: parse_number(json["DilutedEPSTTM"]),
+        quarterly_earnings_growth_yoy: parse_number(json["QuarterlyEarningsGrowthYOY"]),
+        quarterly_revenue_growth_yoy: parse_number(json["QuarterlyRevenueGrowthYOY"]),
+        analyst_target_price: parse_number(json["AnalystTargetPrice"]),
+        trailing_pe: parse_number(json["TrailingPE"]),
+        forward_pe: parse_number(json["ForwardPE"]),
+        price_to_sales_ratio_ttm: parse_number(json["PriceToSalesRatioTTM"]),
+        price_to_book_ratio: parse_number(json["PriceToBookRatio"]),
+        ev_to_revenue: parse_number(json["EVToRevenue"]),
+        ev_to_ebitda: parse_number(json["EVToEBITDA"]),
+        beta: parse_number(json["Beta"]),
+        price_52_week_high: parse_number(json["52WeekHigh"]),
+        price_52_week_low: parse_number(json["52WeekLow"]),
+        price_50_day_moving_average: parse_number(json["50DayMovingAverage"]),
+        price_200_day_moving_average: parse_number(json["200DayMovingAverage"]),
+        shares_outstanding: parse_number(json["SharesOutstanding"]),
+        dividend_date: parse_date(json["DividendDate"], ""),
+        ex_dividend_date: parse_date(json["ExDividendDate"], ""),
+    };
+
+    return overview_data
+}
+
+function transform_raw_json_to_income_statement_data(json: any): Server_data.Income_statement_data {
+
+    const symbol: string = json["symbol"];
+
+    const annual_reports_json = json["annualReports"];
+
+    const entries: Server_data.Income_statement_data_entry[] = [];
+
+    for (const key in annual_reports_json) {
+        const raw_entry = annual_reports_json[key];
+
+        entries.push({
+            fiscal_date_ending: parse_date(raw_entry["fiscalDateEnding"], ""),
+            reported_currency: raw_entry["reportedCurrency"] as string,
+            gross_profit: parse_number(raw_entry["grossProfit"]),
+            total_revenue: parse_number(raw_entry["totalRevenue"]),
+            cost_of_revenue: parse_number(raw_entry["costOfRevenue"]),
+            costof_goods_and_services_sold: parse_number(raw_entry["costofGoodsAndServicesSold"]),
+            operating_income: parse_number(raw_entry["operatingIncome"]),
+            selling_general_and_administrative: parse_number(raw_entry["sellingGeneralAndAdministrative"]),
+            research_and_development: parse_number(raw_entry["researchAndDevelopment"]),
+            operating_expenses: parse_number(raw_entry["operatingExpenses"]),
+            investment_income_net: parse_number(raw_entry["investmentIncomeNet"]),
+            net_interest_income: parse_number(raw_entry["netInterestIncome"]),
+            interest_income: parse_number(raw_entry["interestIncome"]),
+            interest_expense: parse_number(raw_entry["interestExpense"]),
+            non_interest_income: parse_number(raw_entry["nonInterestIncome"]),
+            other_non_operating_income: parse_number(raw_entry["otherNonOperatingIncome"]),
+            depreciation: parse_number(raw_entry["depreciation"]),
+            depreciation_and_amortization: parse_number(raw_entry["depreciationAndAmortization"]),
+            income_before_tax: parse_number(raw_entry["incomeBeforeTax"]),
+            income_tax_expense: parse_number(raw_entry["incomeTaxExpense"]),
+            interest_and_debt_expense: parse_number(raw_entry["interestAndDebtExpense"]),
+            net_income_from_continuing_operations: parse_number(raw_entry["netIncomeFromContinuingOperations"]),
+            comprehensive_income_net_of_tax: parse_number(raw_entry["comprehensiveIncomeNetOfTax"]),
+            ebit: parse_number(raw_entry["ebit"]),
+            ebitda: parse_number(raw_entry["ebitda"]),
+            net_income: parse_number(raw_entry["netIncome"]),
+        });
+    }
+
+    entries.reverse();
+
+    return {
+        symbol: symbol,
+        annual_reports: entries
+    };
+}
+
+function transform_raw_json_to_balance_sheet_data(json: any): Server_data.Balance_sheet_data {
+
+    const symbol: string = json["symbol"];
+
+    const annual_reports_json = json["annualReports"];
+
+    const entries: Server_data.Balance_sheet_data_entry[] = [];
+
+    for (const key in annual_reports_json) {
+        const raw_entry = annual_reports_json[key];
+
+        entries.push({
+            fiscal_date_ending: parse_date(raw_entry["fiscalDateEnding"], ""),
+            reported_currency: raw_entry["reportedCurrency"] as string,
+            total_assets: parse_number(raw_entry["totalAssets"]),
+            total_current_assets: parse_number(raw_entry["totalCurrentAssets"]),
+            cash_and_cash_equivalents_at_carrying_value: parse_number(raw_entry["cashAndCashEquivalentsAtCarryingValue"]),
+            cash_and_short_term_investments: parse_number(raw_entry["cashAndShortTermInvestments"]),
+            inventory: parse_number(raw_entry["inventory"]),
+            current_net_receivables: parse_number(raw_entry["currentNetReceivables"]),
+            total_non_current_assets: parse_number(raw_entry["totalNonCurrentAssets"]),
+            property_plant_equipment: parse_number(raw_entry["propertyPlantEquipment"]),
+            accumulated_depreciation_amortization_ppe: parse_number(raw_entry["accumulatedDepreciationAmortizationPPE"]),
+            intangible_assets: parse_number(raw_entry["intangibleAssets"]),
+            intangible_assets_excluding_goodwill: parse_number(raw_entry["intangibleAssetsExcludingGoodwill"]),
+            goodwill: parse_number(raw_entry["goodwill"]),
+            investments: parse_number(raw_entry["investments"]),
+            long_term_investments: parse_number(raw_entry["longTermInvestments"]),
+            short_term_investments: parse_number(raw_entry["shortTermInvestments"]),
+            other_current_assets: parse_number(raw_entry["otherCurrentAssets"]),
+            other_non_current_assets: parse_number(raw_entry["otherNonCurrentAssets"]),
+            total_liabilities: parse_number(raw_entry["totalLiabilities"]),
+            total_current_liabilities: parse_number(raw_entry["totalCurrentLiabilities"]),
+            current_accounts_payable: parse_number(raw_entry["currentAccountsPayable"]),
+            deferred_revenue: parse_number(raw_entry["deferredRevenue"]),
+            current_debt: parse_number(raw_entry["currentDebt"]),
+            short_term_debt: parse_number(raw_entry["shortTermDebt"]),
+            total_non_current_liabilities: parse_number(raw_entry["totalNonCurrentLiabilities"]),
+            capital_lease_obligations: parse_number(raw_entry["capitalLeaseObligations"]),
+            long_term_debt: parse_number(raw_entry["longTermDebt"]),
+            current_long_term_debt: parse_number(raw_entry["currentLongTermDebt"]),
+            long_term_debt_noncurrent: parse_number(raw_entry["longTermDebtNoncurrent"]),
+            short_long_term_debt_total: parse_number(raw_entry["shortLongTermDebtTotal"]),
+            other_current_liabilities: parse_number(raw_entry["otherCurrentLiabilities"]),
+            other_non_current_liabilities: parse_number(raw_entry["otherNonCurrentLiabilities"]),
+            total_shareholder_equity: parse_number(raw_entry["totalShareholderEquity"]),
+            treasury_stock: parse_number(raw_entry["treasuryStock"]),
+            retained_earnings: parse_number(raw_entry["retainedEarnings"]),
+            common_stock: parse_number(raw_entry["commonStock"]),
+            common_stock_shares_outstanding: parse_number(raw_entry["commonStockSharesOutstanding"]),
+        });
+    }
+
+    entries.reverse();
+
+    return {
+        symbol: symbol,
+        annual_reports: entries
+    };
+}
+
+function transform_raw_json_to_cash_flow_data(json: any): Server_data.Cash_flow_data {
+
+    const symbol: string = json["symbol"];
+
+    const annual_reports_json = json["annualReports"];
+
+    const entries: Server_data.Cash_flow_data_entry[] = [];
+
+    for (const key in annual_reports_json) {
+        const raw_entry = annual_reports_json[key];
+
+        entries.push({
+            fiscal_date_ending: parse_date(raw_entry["fiscalDateEnding"], ""),
+            reported_currency: raw_entry["reportedCurrency"] as string,
+            operating_cashflow: parse_number(raw_entry["operatingCashflow"]),
+            payments_for_operating_activities: parse_number(raw_entry["paymentsForOperatingActivities"]),
+            proceeds_from_operating_activities: parse_number(raw_entry["proceedsFromOperatingActivities"]),
+            change_in_operating_liabilities: parse_number(raw_entry["changeInOperatingLiabilities"]),
+            change_in_operating_assets: parse_number(raw_entry["changeInOperatingAssets"]),
+            depreciation_depletion_and_amortization: parse_number(raw_entry["depreciationDepletionAndAmortization"]),
+            capital_expenditures: parse_number(raw_entry["capitalExpenditures"]),
+            change_in_receivables: parse_number(raw_entry["changeInReceivables"]),
+            change_in_inventory: parse_number(raw_entry["changeInInventory"]),
+            profit_loss: parse_number(raw_entry["profitLoss"]),
+            cashflow_from_investment: parse_number(raw_entry["cashflowFromInvestment"]),
+            cashflow_from_financing: parse_number(raw_entry["cashflowFromFinancing"]),
+            proceeds_from_repayments_of_short_term_debt: parse_number(raw_entry["proceedsFromRepaymentsOfShortTermDebt"]),
+            payments_for_repurchase_of_common_stock: parse_number(raw_entry["paymentsForRepurchaseOfCommonStock"]),
+            payments_for_repurchase_of_equity: parse_number(raw_entry["paymentsForRepurchaseOfEquity"]),
+            payments_for_repurchase_of_preferred_stock: parse_number(raw_entry["paymentsForRepurchaseOfPreferredStock"]),
+            dividend_payout: parse_number(raw_entry["dividendPayout"]),
+            dividend_payout_common_stock: parse_number(raw_entry["dividendPayoutCommonStock"]),
+            dividend_payout_preferred_stock: parse_number(raw_entry["dividendPayoutPreferredStock"]),
+            proceeds_from_issuance_of_common_stock: parse_number(raw_entry["proceedsFromIssuanceOfCommonStock"]),
+            proceeds_from_issuance_of_long_term_debt_and_capital_securities_net: parse_number(raw_entry["proceedsFromIssuanceOfLongTermDebtAndCapitalSecuritiesNet"]),
+            proceeds_from_issuance_of_preferred_stock: parse_number(raw_entry["proceedsFromIssuanceOfPreferredStock"]),
+            proceeds_from_repurchase_of_equity: parse_number(raw_entry["proceedsFromRepurchaseOfEquity"]),
+            proceeds_from_sale_of_treasury_stock: parse_number(raw_entry["proceedsFromSaleOfTreasuryStock"]),
+            change_in_cash_and_cash_equivalents: parse_number(raw_entry["changeInCashAndCashEquivalents"]),
+            change_in_exchange_rate: parse_number(raw_entry["changeInExchangeRate"]),
+            net_income: parse_number(raw_entry["netIncome"]),
+        });
+    }
+
+    entries.reverse();
+
+    return {
+        symbol: symbol,
+        annual_reports: entries
+    };
+}
+
+function transform_csv_to_listing_status_data(csv: string): Server_data.Listing_status_entry[] {
+
+    const entries: Server_data.Listing_status_entry[] = [];
+
+    const lines = csv.split("\r\n");
+
+    for (let index = 1; index < lines.length; ++index) {
+        const line = lines[index];
+
+        const values = line.split(",");
+
+        entries.push({
+            symbol: parse_string(values[0]),
+            name: parse_string(values[1]),
+            exchange: parse_string(values[2]),
+            asset_type: parse_string(values[3]),
+            ipo_date: parse_date(values[4], ""),
+            delisting_date: values[5] === "null" ? null : parse_date(values[5], ""),
+            status: parse_string(values[6]),
+        });
+    }
+
+    return entries;
 }
